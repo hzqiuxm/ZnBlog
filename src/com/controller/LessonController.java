21f3fca727b336318d3f46609c1637b053334b03
home <home@163.com>
2015-08-19 15:29:56 +0800

    新增课程控制类

---
+++ src/com/controller/LessonController.java
@@ -0,0 +1,14 @@
+package com.controller;
+
+import com.jfinal.core.Controller;
+
+/**
+ * Created by hzqixm on 2015/8/19.
+ */
+public class LessonController extends Controller {
+
+    public void show_lesson(){
+
+    }
+
+}
